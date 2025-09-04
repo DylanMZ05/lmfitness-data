1692c8c3d8ee6d73d27565f037b41ba50bb256b5
DylanMZ <dylanmazurzgainer@gmail.com>
2025-09-03 22:00:00 -0300

    a

--- src/components/WspButton.tsx
+++ src/components/WspButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FaWhatsapp } from "react-icons/fa";
 
 const WspButton: React.FC = () => {
-  const phoneNumber = "+54 9 2257 53-1656";
+  const phoneNumber = "+5492257531656";
   const message = "¡Hola! Me gustaría hacer una consulta.";
 
   const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
